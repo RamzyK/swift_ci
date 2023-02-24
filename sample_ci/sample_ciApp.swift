a240ffc46088c70a9f62b0c834941b4ecf6c43a4
Ramzy Kermad <kermad.r@sncf.fr>
2023-02-24 19:34:01 +0100

    Initial Commit

---
+++ sample_ci/sample_ciApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_ciApp.swift
+//  sample_ci
+//
+//  Created by 9822072R on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct sample_ciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
